f6aa4a2f5f3d07aa08f211385caa9dc72727633e
sali-web <salina.gurung009@gmail.com>
2022-10-11 19:18:55 +0545

    Assignment1 committed

---
+++ assignment1.js
@@ -0,0 +1,4 @@
+function kbtomb(data){
+    console.log(data*0.001);
+}
+module.exports.kbtomb1 = kbtomb;
---
+++ test.js
@@ -0,0 +1,3 @@
+var result = require('./assignment1');
+//const input = prompt ("Enter value in kb to be converted in mb");
+result.kbtomb1(1024);
